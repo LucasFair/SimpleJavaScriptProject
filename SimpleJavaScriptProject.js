6314f923ca0ad0aef1d2dbad3f0745fba2710d83
LucasFair <141710445+LucasFair@users.noreply.github.com>
2024-04-15 09:43:19 +0200

    Initial upload.

---
+++ SimpleJavaScriptProject.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JavaScript</title>
+    </head>
+<body>
+
+<h2>
+    Simple JavaScript Project
+</h2>
+
+<script>
+let a,b,c;
+
+a = 6;  // Edit this to change the value of 'a'
+b = 13;  // Edit this to change the value of 'b'
+c = a + b;
+</script>
+
+<p>
+    An easy calculation that where 'a' and 'b' can be changed.
+<p>
+    <script>
+    document.write('a = ' + a);
+    </script>    
+</p>
+    <script>
+    document.write('b = ' + b);
+    </script> 
+<p>
+    c = a + b
+</p>
+</p>
+
+<script>
+    document.write('The sum of a (' + a + ') + b (' + b + ') = ' + c);
+</script>
+
+</body>
+</html>
